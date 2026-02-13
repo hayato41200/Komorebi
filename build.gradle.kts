11640eeef0b3a29e58ba406cb0205727bb4ec3d6
hayato41200 <hayatochida0412@icloud.com>
2026-02-13 12:24:50 +0900

    Upgrade Android Gradle plugin version to 8.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
 // ここに同じIDを2回書かないようにします
-    id("com.android.application") version "9.0.0" apply false
-    id("com.android.library") version "9.0.0" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.54" apply false
